07c80be32cb0a820d7df40adb558674e137c5682
pecata <pecata83@gmail.com>
2025-08-13 16:33:27 +0300

    feat: add bugsnag

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,31 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+import Bugsnag from '@bugsnag/js';
+import BugsnagPerformance from '@bugsnag/browser-performance';
+import { BugsnagErrorHandler } from '@bugsnag/plugin-angular';
+import { ErrorHandler, Provider } from '@angular/core';
+
+// ToDo Move keys to environment variables for production
+Bugsnag.start({
+  apiKey: '77911f5aeee17c88421409d33d569ee2',
+});
+
+BugsnagPerformance.start({
+  apiKey: '77911f5aeee17c88421409d33d569ee2'
+});
+
+
+const bugsnagProvider: Provider = {
+  provide: ErrorHandler,
+  useFactory: () => new BugsnagErrorHandler()
+};
+
+bootstrapApplication(AppComponent, {
+  ...appConfig,
+  providers: [
+    ...(appConfig.providers || []),
+    bugsnagProvider
+  ]
+}).catch((err) => console.error(err));
